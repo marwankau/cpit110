fb69e3aeea4a49ba36b7353a311625913ee19b0f
Marwan Alsahafi KAU 471 302 <mnalsahafi@kau.edu.sa>
2022-11-01 17:44:13 +0300

    ch6 finished

---
+++ ch6/s80.py
@@ -0,0 +1,16 @@
+def getGrade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+
+s = eval(input("Enter your score: "))
+print("Your grade is", getGrade(s))
+
